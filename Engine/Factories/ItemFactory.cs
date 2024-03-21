a75e29a8357ef8d08880701e30691e5616f7faf2
ItsKhen <156722144+ItsKhen@users.noreply.github.com>
2024-03-21 17:11:36 +0800

    Updated to latest activity

---
+++ Engine/Factories/ItemFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    internal static class ItemFactory
+    {
+        private static List<GameItem> _standardGameItems;
+
+        static ItemFactory()
+        {
+            _standardGameItems = new List<GameItem>();
+            _standardGameItems.Add(new Weapon(1001, "Pointy stick", 1, 0, 2));
+            _standardGameItems.Add(new Weapon(1002, "Rusty sword", 5, 1, 3));
+            _standardGameItems.Add(new GameItem(9001, "Snake fang", 1));
+            _standardGameItems.Add(new GameItem(9002, "Snakeskin", 2));
+            _standardGameItems.Add(new GameItem(9003, "Rat tail", 1));
+            _standardGameItems.Add(new GameItem(9004, "Rat fur", 2));
+            _standardGameItems.Add(new GameItem(9005, "Spider fang", 3));
+            _standardGameItems.Add(new GameItem(9006, "Spider silk", 5));
+        }
+
+        public static GameItem CreateGameItem(int itemID)
+        {
+            GameItem standardGameItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeID == itemID);
+
+            if (standardGameItem != null)
+            {
+                if (standardGameItem is Weapon)
+                {
+                    return (standardGameItem as Weapon).Clone();
+                }
+
+                return (standardGameItem.Clone());
+            }
+
+            return null;
+        }
+    }
+}
--- Engine/obj/Debug/net8.0/Engine.AssemblyInfo.cs
+++ Engine/obj/Debug/net8.0/Engine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Engine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fafba82c671a063b47e43a3879303acfac1b6af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a36486db871ceedcf4ed1315a59e03b1ddb6259d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Engine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Engine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
