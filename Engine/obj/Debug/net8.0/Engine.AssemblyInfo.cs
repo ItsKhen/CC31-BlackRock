7ec051f700754d77df2cd7e9ea2e87f6d8ed36e8
ItsKhen <156722144+ItsKhen@users.noreply.github.com>
2024-04-05 21:16:01 +0800

    Debug

--- Engine/obj/Debug/net8.0/Engine.AssemblyInfo.cs
+++ Engine/obj/Debug/net8.0/Engine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Engine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e82de9ff1f116f3a57363e2719cde8903925998d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fde969f48824aa87bc6941e886064e710550386")]
 [assembly: System.Reflection.AssemblyProductAttribute("Engine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Engine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
