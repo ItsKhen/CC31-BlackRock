d1e18f669cf4f348e3ce912356ca6ef5790b3b48
ItsKhen <156722144+ItsKhen@users.noreply.github.com>
2024-04-09 12:39:01 +0800

    Fixed Borders needs functions

--- Game/obj/Debug/net8.0-windows/CC31B-Blackrock.AssemblyInfo.cs
+++ Game/obj/Debug/net8.0-windows/CC31B-Blackrock.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CC31B-Blackrock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dcaf3c2ccb2e61debbe18dd135a2e2e1d905110")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca1e4e4d26850544d40b21240e389c3e670fe958")]
 [assembly: System.Reflection.AssemblyProductAttribute("CC31B-Blackrock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CC31B-Blackrock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
